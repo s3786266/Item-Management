0982f017b4e8cc756993d273d7c8c3d294b9d470
Daniel <s3786266@student.rmit.edu.au>
2022-09-09 01:28:11 +1000

    Updated controller endpoints and wired it to the dao layer

--- src/main/java/com/rmit/sept/itemmanagement/controller/ItemController.java
+++ src/main/java/com/rmit/sept/itemmanagement/controller/ItemController.java
@@ -1,40 +1,40 @@
 package com.rmit.sept.itemmanagement.controller;
 
-import com.rmit.sept.itemmanagement.dao.ItemService;
+import com.rmit.sept.itemmanagement.dao.ItemDAO;
 import com.rmit.sept.itemmanagement.model.Item;
+import com.rmit.sept.itemmanagement.model.Items;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ItemController {
 
     @Autowired
-    ItemService itemService;
+    ItemDAO itemDAO;
 
     @PostMapping("/item")
-    public ResponseEntity<Item> createItem (@RequestBody Item itemDetails) {
-        return itemService.saveItemDetails(itemDetails);
+    public Item createItem (@RequestBody Item itemDetails) {
+        return itemDAO.saveItemDetails(itemDetails);
     }
 
     @GetMapping ("/item/item{id}")
-    public ResponseEntity<Item> getItemDetails (@PathVariable String id) {
-        return itemService.getItemDetails(id);
+    public Item getItemDetails (@PathVariable Integer id) {
+        return itemDAO.getItemDetails(id);
     }
 
     @GetMapping("/item/item")
-    public ResponseEntity<Item> getItemListDetails () {
-        return itemService.getItemListDetails();
+    public Items getItemListDetails () {
+        return itemDAO.getItemDetails();
     }
 
-    @PutMapping("/item/item")
-    public ResponseEntity<Item> updateItemDetails(@RequestBody Item itemDetails) {
-        return itemService.updateItemDetails(itemDetails);
+    @PutMapping("/item/item{id}")
+    public Item updateItemDetails(@RequestBody Item itemDetails) {
+        return itemDAO.updateItemDetails(itemDetails);
     }
 
     @DeleteMapping("/item/item")
-    public ResponseEntity<Item> deleteItemDetails() {
-        return itemService.deleteItemDetails();
+    public Item deleteItemDetails() {
+        return itemDAO.deleteItemDetails();
     }
 
 }
