67b201705e077ac53330458323b451a227ecd8b6
Daniel <s3786266@student.rmit.edu.au>
2022-09-08 22:55:17 +1000

    Create the items entity to store the list of items

--- src/main/java/com/rmit/sept/itemmanagement/model/Items.java
+++ src/main/java/com/rmit/sept/itemmanagement/model/Items.java
@@ -1,4 +1,20 @@
 package com.rmit.sept.itemmanagement.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Items {
+
+    private List<Item> itemList;
+
+    public List<Item> getTeacherList() {
+        if(itemList == null) {
+            itemList = new ArrayList<>();
+        }
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
 }
