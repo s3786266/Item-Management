8f620a08248c62061c5f646e4513d25ed9137963
Daniel <s3786266@student.rmit.edu.au>
2022-09-09 01:31:45 +1000

    refracted the item entities and added new methods

--- src/main/java/com/rmit/sept/itemmanagement/model/Items.java
+++ src/main/java/com/rmit/sept/itemmanagement/model/Items.java
@@ -1,20 +1,43 @@
 package com.rmit.sept.itemmanagement.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Items {
 
-    private List<Item> itemList;
+//    private List<Item> itemList;
 
-    public List<Item> getTeacherList() {
+    public List<Item> itemList;
+
+    public List<Item> getItemList() {
         if(itemList == null) {
             itemList = new ArrayList<>();
         }
         return itemList;
     }
 
-    public void setItemList(List<Item> itemList) {
-        this.itemList = itemList;
+    public Item updateItemDetails(Item itemDetails){
+        itemDetails.setId(itemDetails.getId());
+        itemDetails.setName(itemDetails.getName());
+        itemDetails.setDesc(itemDetails.getDesc());
+        itemDetails.setImagePath(itemDetails.getImagePath());
+        itemDetails.setPrice(itemDetails.getPrice());
+        return itemDetails;
     }
+
+    public Item saveItemDetails(Item itemDetails){
+        return itemDetails;
+    }
+
+//    public Item deleteItemDetails(){
+//        return null;
+//    }
+
 }
