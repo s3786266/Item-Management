d4d317d7be65f61e241f5fcf08e973909d719659
Daniel <s3786266@student.rmit.edu.au>
2022-09-08 21:55:30 +1000

    created the required methods for controller endpoints

--- src/main/java/com/rmit/sept/itemmanagement/dao/ItemService.java
+++ src/main/java/com/rmit/sept/itemmanagement/dao/ItemService.java
@@ -1,4 +1,23 @@
 package com.rmit.sept.itemmanagement.dao;
 
+import com.rmit.sept.itemmanagement.model.Item;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemService {
+    public ResponseEntity<Item> saveItemDetails(Item itemDetails) {
+    }
+
+    public ResponseEntity<Item> getItemDetails(String id) {
+    }
+
+    public ResponseEntity<Item> getItemListDetails() {
+    }
+
+    public ResponseEntity<Item> updateItemDetails(Item itemDetails) {
+    }
+
+    public ResponseEntity<Item> deleteItemDetails() {
+    }
 }
