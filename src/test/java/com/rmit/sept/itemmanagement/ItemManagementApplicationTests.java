fc73d1f9e4a0b352066967844b6021c123b57c6f
Daniel <s3786266@student.rmit.edu.au>
2022-09-08 21:05:55 +1000

    committed the auto generated spring boot files

---
+++ src/test/java/com/rmit/sept/itemmanagement/ItemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.itemmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
