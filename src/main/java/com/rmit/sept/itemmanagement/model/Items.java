d6dcaad3af858ed14f7bf6e462c193b4343b4da8
Daniel <s3786266@student.rmit.edu.au>
2022-09-09 01:25:36 +1000

    Created basic items entity

---
+++ src/main/java/com/rmit/sept/itemmanagement/model/Items.java
@@ -0,0 +1,12 @@
+package com.rmit.sept.itemmanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Items {
+}
