d26d6aacdf5eff0811c6a1e93d7c635afcfc5efe
Daniel Gao <48667721+s3786266@users.noreply.github.com>
2022-09-09 14:08:34 +1000

    Created the business logic for the DAO methods

--- src/main/java/com/rmit/sept/itemmanagement/dao/ItemDAO.java
+++ src/main/java/com/rmit/sept/itemmanagement/dao/ItemDAO.java
@@ -3,9 +3,12 @@ package com.rmit.sept.itemmanagement.dao;
 import com.rmit.sept.itemmanagement.model.Item;
 import com.rmit.sept.itemmanagement.model.Items;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
+@Component
 @Data
 public class ItemDAO {
 
@@ -13,29 +16,28 @@ public class ItemDAO {
 
     static
     {
-        itemDetails.getItemList().add(new Item(01, "item 1", "this is item 1", "//image-path",12.0));
-        itemDetails.getItemList().add(new Item(02, "item 2", "this is item 2", "//image-path", 13.0));
-        itemDetails.getItemList().add(new Item(03, "item 3", "this is item 3", "//image-path", 14.0));
+        itemDetails.getItemList().add(new Item("01", "item 1", "this is item 1", "//image-path",12.0));
+        itemDetails.getItemList().add(new Item("02", "item 2", "this is item 2", "//image-path", 13.0));
+        itemDetails.getItemList().add(new Item("03", "item 3", "this is item 3", "//image-path", 14.0));
     }
 
     public Item saveItemDetails(Item newItemDetails) {
         return itemDetails.saveItemDetails(newItemDetails);
     }
 
-    public Item getItemDetails(Integer id) {
-        return itemDetails.getItemList().get(id);
+    public Item getItemDetailsById(String itemId) {
+        return itemDetails.getItemDetailsById(itemId);
     }
 
-    public Items getItemDetails() {
+    public Items getAllItemListDetails() {
         return itemDetails;
     }
 
-    public Item updateItemDetails(Item newItemDetails) {
-        return itemDetails.updateItemDetails(newItemDetails);
+    public Item updateItemDetails(Item newItemDetails, String itemId) {
+        return itemDetails.updateItemDetails(newItemDetails, itemId);
     }
 
-    public Item deleteItemDetails() {
-//        return itemDetails.deleteItemDetails();
+    public Item deleteItemDetails(String itemId) {
+        return itemDetails.deleteItemDetailsById(itemId);
     }
 }
-
